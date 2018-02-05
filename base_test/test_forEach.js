16731640dbae9e940c95555061944ed825702ef7
CNBlackJ <yes.heng@icloud.com>
2018-02-05 15:31:43 +0800

    test for loop break and return

--- base_test/test_forEach.js
+++ base_test/test_forEach.js
@@ -3,3 +3,20 @@
 	a.b.forEach((res) => {
 		console.log(res);
 	})
+
+	const arr = [1,2,3,4,5,6]
+
+	arr.forEach((res) => {
+		if (res === 3) {
+			return
+		}
+		console.log('return: ', res)
+	})
+
+	for (let i = 0; i < arr.length; i++) {
+		const res = arr[i]
+		if (res === 3) {
+			break
+		}
+		console.log('break: ', res)
+	}
